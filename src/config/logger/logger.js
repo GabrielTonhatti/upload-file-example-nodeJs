8b59978add48770e78a862c7554542fed4ace5be
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-12 09:53:02 -0300

    Atualiza logger

--- src/config/logger/logger.js
+++ src/config/logger/logger.js
@@ -1,20 +1,33 @@
 const pino = require("pino");
 const path = require("path");
 
-const logger = pino({
-    name: "upload example",
-    level: "info",
-    transport: {
-        target: "./pino-pretty-transport",
-        options: {
-            colorize: true,
-            levelFirst: true,
-            sync: true,
-            translateTime: true,
-            singleLine: false,
-            ignore: "pid,filename",
+const enviroment = process.env.NODE_ENV;
+
+let logger;
+
+if (enviroment !== "production") {
+    logger = pino({
+        name: "upload example",
+        level: "info",
+        transport: {
+            target: "./pino-pretty-transport",
+            options: {
+                colorize: true,
+                levelFirst: true,
+                sync: true,
+                translateTime: true,
+                singleLine: false,
+                ignore: "pid,filename",
+            },
         },
-    },
-}).child({ filename: path.basename(__filename) });
+    });
+} else {
+    logger = pino({
+        name: "upload example",
+        level: "info",
+    });
+}
+
+logger.child({ filename: path.basename(__filename) });
 
 module.exports = logger;
