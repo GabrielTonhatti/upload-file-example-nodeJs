2e2a49d0d366bb1486064fa8379fee355730cac3
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2022-08-11 23:24:18 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ const logger = Utils.getLoggerWithPathFile(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
 app.use(
     "/files",
     express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
